2acd131d0a83ced4af3f21e3a26349eebcf13405
justanotheranon <gamedesignwithdee@gmail.com>
2015-01-14 22:37:25 +0100

    Victory state graphics

--- DeadState.cpp
+++ DeadState.cpp
@@ -10,7 +10,9 @@
 DeadState::DeadState(System& systems)
 {
 	m_systems = systems;
-	SDL_SetRenderDrawColor(m_systems.draw_manager->GetRenderer(), 255, 0, 0, 255);
+	SDL_SetRenderDrawColor(m_systems.draw_manager->GetRenderer(), 111, 1, 1, 255);
+
+	//Creates death prompt sprite
 	SpriteAnimation* sprite = m_systems.sprite_manager->CreateAnimatedSprite("../Skelly_Dungeon/assets/youdied.txt");
 	sprite->SetAnimation("youdied");
 	m_animsprite.push_back(sprite);
@@ -23,9 +25,14 @@ DeadState::~DeadState()
 
 bool DeadState::Update(float deltatime)
 {
+	//Scales the death prompt sprite with time
 	m_scale += deltatime;
 
 	if (m_systems.input_manager->GetKeyboard()->IsKeyDown(5) == true)
+	{
+		return false;
+	}
+	else if (m_scale > 4)
 	{
 		return false;
 	}
@@ -34,7 +41,8 @@ bool DeadState::Update(float deltatime)
 
 void DeadState::Draw()
 {
-	m_systems.draw_manager->Draw(m_animsprite[0], 342, 10, m_scale);
+	//Draws the scaling death prompt sprite
+	m_systems.draw_manager->Draw(m_animsprite[0], 10, 10, m_scale);
 }
 
 State* DeadState::NextState()
--- VictoryState.cpp
+++ VictoryState.cpp
@@ -4,12 +4,25 @@
 #include "Keyboard.h"
 #include "InputManager.h"
 #include "DrawManager.h"
+#include "SpriteManager.h"
 
 
 VictoryState::VictoryState(System& systems)
 {
 	m_systems = systems;
 	SDL_SetRenderDrawColor(m_systems.draw_manager->GetRenderer(), 0, 255, 0, 255);
+
+	//Creates victory prompt sprites
+	SpriteAnimation* sprite = m_systems.sprite_manager->CreateAnimatedSprite("../Skelly_Dungeon/assets/victory.txt");
+	sprite->SetAnimation("victory");
+	m_animsprite.push_back(sprite);
+
+	sprite->SetAnimation("elipses");
+	m_animsprite.push_back(sprite);
+
+	sprite->SetAnimation("fornow");
+	m_animsprite.push_back(sprite);
+
 }
 
 
@@ -28,7 +41,10 @@ bool VictoryState::Update(float deltatime)
 
 void VictoryState::Draw()
 {
-
+	//Draws victory prompt sprites
+	m_systems.draw_manager->Draw(m_animsprite[0], 10, 10, 1);
+	m_systems.draw_manager->Draw(m_animsprite[1], 454, 180, 1);
+	m_systems.draw_manager->Draw(m_animsprite[2], 387, 355, 1);
 }
 
 State* VictoryState::NextState()
